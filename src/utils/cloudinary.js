2230e8d15c7889c43d7cd9004c2d32caf47e5c43
khushbu042 <khushbumourya066@gmail.com>
2024-07-21 22:46:34 +0530

    cloudinary utility complete

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs" 
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_API_KEY,  
+    api_secret: process.env.CLOUD_API_SECRET 
+});
+
+const uploadONCloudinary =  async (localFilePath) => {
+    try {
+        if(! localFilePath)  return null;
+        // upload file on cloudinary
+        const uploadResult = await cloudinary.uploader.upload(
+            localFilePath, {resource_type : auto}
+        )
+        //upload file sucessfully on cloudinary
+        fs.unlinkSync(localFilePath);
+        return uploadResult;
+    }
+    catch (error){
+        fs.unlinkSync(localFilePath)  / // remove the locally saved temporary file as the upload got failed
+        console.oog(error);
+        return null; 
+    }
+
+}
+
+export {uploadONCloudinary}
+
+
+
